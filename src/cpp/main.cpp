77b3b0ad57d704160cfa18b4edfb60125251ba52
MadinaBahriddinova <127374347+MadinaBahriddinova@users.noreply.github.com>
2024-01-17 10:17:39 +0500

    Update main.cpp

--- src/cpp/main.cpp
+++ src/cpp/main.cpp
@@ -61,6 +61,12 @@ int main() {
         std::cout << "False" << std::endl;
     }
 
+ //task6
+    std::vector<std::vector<int>> times = {{2, 1, 1}, {2, 3, 1}, {3, 4, 1}};
+    int n = 4;
+    int k = 2;
+    int result = networkDelayTime(times, n, k);
+    std::cout << result << std::endl;
 
  
 
