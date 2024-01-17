54b5c553573e35824c6b8f7a161c63e5a371de46
MadinaBahriddinova <127374347+MadinaBahriddinova@users.noreply.github.com>
2024-01-17 10:01:19 +0500

    Update task_5.cpp

--- src/cpp/task_5.cpp
+++ src/cpp/task_5.cpp
@@ -4,5 +4,35 @@
  * Name:
  */
 
-class Problem5 {
-};
+#include <iostream>
+#include <vector>
+
+bool hasZeroSumTriplet(const std::vector<int>& numbers) {
+    int n = numbers.size();
+
+    for (int i = 0; i < n - 2; ++i) {
+        for (int j = i + 1; j < n - 1; ++j) {
+            for (int k = j + 1; k < n; ++k) {
+                if (numbers[i] + numbers[j] + numbers[k] == 0) {
+                    return true;  
+                }
+            }
+        }
+    }
+
+    return false;  
+}
+
+int main() {
+    std::vector<int> numbers = {-5, -17, 7, -4, 3, -2, 4};
+
+    bool result = hasZeroSumTriplet(numbers);
+
+    if (result) {
+        std::cout << "True" << std::endl;
+    } else {
+        std::cout << "False" << std::endl;
+    }
+
+    return 0;
+}
