2acdc2e8b14fdcb6e3602ff69953fd0222d3405f
MadinaBahriddinova <127374347+MadinaBahriddinova@users.noreply.github.com>
2024-01-17 10:11:01 +0500

    Update main.cpp

--- src/cpp/main.cpp
+++ src/cpp/main.cpp
@@ -14,10 +14,31 @@
 #include "task_5.cpp"
 #include "task_6.cpp"
 
-using namespace std;
 
 int main() {
-    // write your code here
+    
+    //task1
+    std::vector<int> input = {0, 3, 1, 0, 4, 5, 2, 0};
+    ListNode* head = new ListNode(input[0]);
+    ListNode* current = head;
+    for (int i = 1; i < input.size(); ++i) {
+        current->next = new ListNode(input[i]);
+        current = current->next;
+    }
+
+    
+    head = mergeZeros(head);
+
+    
+    displayLinkedList(head);
+    while (head) {
+        ListNode* temp = head;
+        head = head->next;
+        delete temp;
+    }
+
+
+ 
 
     return 0;
 }
